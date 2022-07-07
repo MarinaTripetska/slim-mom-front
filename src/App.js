f8c58254aab530ade3b7b28abe59c69886765faa
Dartena <82412028+Dartena@users.noreply.github.com>
2022-07-08 02:03:39 +0300

    Modal dom and styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+import Modal from 'pages/Modal';
+
 function App() {
+  const [openModal, setOpenModal] = useState(false);
+
+  const closeModal = () => {
+    setOpenModal(false);
+  };
+
   return (
     <div>
       <p>SlimMom START TEST!</p>
+      <button //Тимчасова кнопка
+        onClick={() => {
+          setOpenModal(true);
+        }}
+      >
+        open modal
+      </button>
+      {!openModal || <Modal closeModalHandle={closeModal} />}
     </div>
   );
 }
