85fe2b0f37b8ae01266a08de9e955eed22ece5ba
OlhaNester <olga_nv@ukr.net>
2022-07-10 12:14:56 +0300

    fix container

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -1,16 +1,17 @@
+import { breakpoints } from 'assets/sizes';
 import styled from 'styled-components';
 
 export const ContainerWrapper = styled.div`
   margin: auto;
-  @media screen and (max-width: 767px) {
+  @media ${breakpoints.maxTablet}
     width: 320px;
     padding: 0 20px;
   }
-  @media screen and (min-width: 768px) {
+  @media  ${breakpoints.tablet} {
     width: 768px;
     padding: 0 32px;
   }
-  @media screen and (min-width: 1280px) {
+  @media  ${breakpoints.desktop} {
     width: 1280px;
     padding: 0 16px;
   }
