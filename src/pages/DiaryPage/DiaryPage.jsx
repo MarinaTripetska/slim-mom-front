48f9a4ce0dee847211bc4eac2fa8e644752bc196
Marina Tripetska <marina.tripetskaya@gmail.com>
2022-07-16 20:44:06 +0200

    fix: syntax

--- src/pages/DiaryPage/DiaryPage.jsx
+++ src/pages/DiaryPage/DiaryPage.jsx
@@ -4,22 +4,27 @@ import { useDispatch, useSelector } from 'react-redux';
 import DiaryDateCalendar from 'components/DiaryDateCalendar';
 import DiaryAddProductForm from '../../components/DiaryAddProductForm';
 import DiaryProductsList from '../../components/DiaryProductsList';
-import MobileSidebar from '../../components/MobileSidebar'
-import {AddBtnMobile,PageWrap,SidebarWrap,ListWrap} from './DiaryPageStyle';
+import MobileSidebar from '../../components/MobileSidebar';
+import {
+  AddBtnMobile,
+  PageWrap,
+  SidebarWrap,
+  ListWrap,
+} from './DiaryPageStyle';
 import SideBar from 'components/SideBar';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 import { addProduct } from '../../redux/app/products/productsOperation';
-import {getProductsList} from '../../redux/app/products/productsSelectors';
+import { getProductsList } from '../../redux/app/products/productsSelectors';
 import Header from 'components/Header';
 import Container from 'components/Container';
 
 export default function DiaryPage() {
   const [mobileAddSelected, setMobileAddSelected] = useState(false);
-  const productsList = useSelector(getProductsList); 
+  const productsList = useSelector(getProductsList);
   const dispatch = useDispatch();
-  console.log(productsList)
-  
+  console.log(productsList);
+
   const formSubmitHandler = data => {
     const { product, weight } = data;
 
@@ -32,45 +37,46 @@ export default function DiaryPage() {
     setMobileAddSelected(false);
   };
   return (
+    <>
+      <Header localPage="DiaryPage" />
+      <PageWrap>
+        <MobileSidebar
+          onGoBack={() => setMobileAddSelected(false)}
+          mobileAddSelected={mobileAddSelected}
+        />
+        <Container>
+          <DiaryDateCalendar />
+          <DiaryAddProductForm
+            onSubmit={formSubmitHandler}
+            className={mobileAddSelected ? '' : 'hideOnMobile'}
+          />
+          <ListWrap className={mobileAddSelected ? 'hideOnMobile' : ''}>
+            {<DiaryProductsList products={productsList} />}
+          </ListWrap>
+          {!mobileAddSelected && (
+            <AddBtnMobile
+              className={'showOnMobile'}
+              onClick={() => setMobileAddSelected(true)}
+            >
+              <BsPlusLg size={14} />
+            </AddBtnMobile>
+          )}
+        </Container>
+        <SidebarWrap className={mobileAddSelected ? 'hideOnMobile' : ''}>
+          <SideBar date="16.07.2022" />
+        </SidebarWrap>
+      </PageWrap>
+    </>
 
-<>
-<Header localPage="DiaryPage" />
-<PageWrap>
-<MobileSidebar
-onGoBack={() => setMobileAddSelected(false)}
-mobileAddSelected={mobileAddSelected}
-/>
-  <Container>
-    <DiaryDateCalendar />
-    <DiaryAddProductForm
-      onSubmit={formSubmitHandler}
-      className={mobileAddSelected ? '' : 'hideOnMobile'}/>
-    <ListWrap className={mobileAddSelected ? 'hideOnMobile' : ''}>
-      {<DiaryProductsList products={productsList} />}
-    </ListWrap>
-      {!mobileAddSelected&&(
-        <AddBtnMobile
-      className={'showOnMobile'}
-      onClick={() => setMobileAddSelected(true)}>
-      <BsPlusLg size={14} />
-    </AddBtnMobile>)}
-  </Container>
-  <SidebarWrap className={mobileAddSelected ? 'hideOnMobile' : ''}>
-    <SideBar date="16.07.2022" />
-  </SidebarWrap>
-  </PageWrap>
-</>
-
-   {// <>
+    // <>
     //  <Header localPage="DiaryPage" />
-   //   <DiaryDateCalendar />
+    //   <DiaryDateCalendar />
     //  <DiaryAddProductForm
     //    onSubmit={formSubmitHandler}
     //    className={mobileAddSelected ? '' : 'hideOnMobile'}
     //  />
     //  <DiaryProductsList products={productsList} />
     //  <SideBar date="16.07.2022" />
-  //  </>}
-
+    //  </>
   );
 }
