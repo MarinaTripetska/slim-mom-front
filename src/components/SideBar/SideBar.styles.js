aa5a8424b27ac40e61dd487552f5667b801a59c6
OlhaNester <olga_nv@ukr.net>
2022-07-17 16:44:19 +0300

    fix

--- src/components/SideBar/SideBar.styles.js
+++ src/components/SideBar/SideBar.styles.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 import bgi from '../../assets/images/leaves-desktop.png';
+import bgTablet from '../../assets/images/leaves-aside-tablet.png';
+import { breakpoints } from '../../assets/sizes';
 
 const SideBarDiv = styled.div`
   background: #f0f1f3;
-  background-image: url(${bgi});
+
   margin: auto;
 
   background-repeat: no-repeat;
@@ -16,7 +18,7 @@ const SideBarDiv = styled.div`
   align-items: center;
   justify-content: center;
   @media (max-width: 1279px) {
-    height: auto;
+    height: 100%;
     max-height: fit-content;
     max-width: 100vw;
     flex-direction: row;
@@ -30,15 +32,32 @@ const SideBarDiv = styled.div`
     align-items: center;
     padding: 40px 20px;
   }
+  @media ${breakpoints.onlyTablet} {
+    background-image: url(${bgTablet});
+    background-repeat: no-repeat;
+    background-position: bottom right;
+    background-size: 428px 602px;
+    height: 326px;
+    padding-top: 0px;
+    padding-bottom: 0px;
+  }
 `;
 
 const ListTitle = styled.h3`
-  margin-top: 0;
-  margin-bottom: 40px;
+  // margin-bottom: 40px;
+  // margin-bottom: 20px;
   font-size: inherit;
   line-height: inherit;
   @media (max-width: 550px) {
-    margin-bottom: 24px;
+    margin-bottom: 20px;
+  }
+  @media ${breakpoints.onlyTablet} {
+    margin-top: 0;
+  }
+`;
+const ProdDiv = styled.div`
+  @media ${breakpoints.onlyTablet} {
+    margin-left: 97px;
   }
 `;
 
@@ -50,10 +69,18 @@ const ProdListDiv = styled.div`
   @media (max-width: 550px) {
     width: 100%;
   }
+  @media ${breakpoints.onlyTablet} {
+    display: flex;
+    width: 100%;
+
+    padding: 80px 32px;
+  }
 `;
 
 const ProdList = styled.ul`
   color: #9b9faa;
+  @media (max-width: 767px) {
+    margin-bottom: 40px;
 `;
 
-export { SideBarDiv, ProdListDiv, ProdList, ListTitle };
+export { SideBarDiv, ProdListDiv, ProdList, ListTitle, ProdDiv };
