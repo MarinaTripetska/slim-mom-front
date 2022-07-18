02c2f01f9835a2f6ae12a6a5a1f26325810591ae
Viktor Kaida <82412028+Dartena@users.noreply.github.com>
2022-07-18 18:33:58 +0300

    litle fix

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { breakpoints } from 'assets/sizes';
 
 export const HeaderStyled = styled.header`
-
   position: static;
   top: 0;
   /* height: 80px; */
@@ -24,12 +23,12 @@ export const HeaderStyled = styled.header`
   @media ${breakpoints.desktop} {
     border-bottom: none;
     padding: 80px 16px;
-
+  }
+`;
 export const PositionRoot = styled.div`
   position: static;
   padding-top: 80px;
   @media ${breakpoints.desktop} {
     padding-top: 0px;
-
   }
 `;
