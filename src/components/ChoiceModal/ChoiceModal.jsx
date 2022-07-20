52db91757cc75053acf9c85b9c2b2c55db8c09f1
Falconoff <sokolovserega1982@gmail.com>
2022-07-20 09:37:30 +0300

    add agree-modals for logout and prod-removing

---
+++ src/components/ChoiceModal/ChoiceModal.jsx
@@ -0,0 +1,80 @@
+import { useEffect } from 'react';
+import {
+  Overlay,
+  ModalDiv,
+  ModalTtl,
+  Text,
+  CloseModalBtn,
+  ContentWrap,
+  BtnWrapper,
+} from './ChoiceModal.styled';
+import Button from '../Button';
+import CloseBtn from '../CloseBtn/CloseBtn';
+
+function ChoiceModal({ text, choiceHandler, closeModalHandle, subText }) {
+  useEffect(() => {
+    window.addEventListener('keydown', escKeyHandle);
+    return () => {
+      window.removeEventListener('keydown', escKeyHandle);
+    };
+  }, []);
+
+  const escKeyHandle = event => {
+    if (event.keyCode === 27) {
+      choiceHandler(false);
+      closeModalHandle();
+    }
+  };
+  const onClickOvrlHandle = event => {
+    if (event.target.id === 'modal-overlay') {
+      choiceHandler(false);
+      closeModalHandle();
+    }
+  };
+
+  const onOkClickHandle = () => {
+    choiceHandler(true);
+    closeModalHandle();
+  };
+
+  const onCancelClickHandle = () => {
+    choiceHandler(false);
+    closeModalHandle();
+  };
+
+  const closeModal = () => {
+    choiceHandler(false);
+    closeModalHandle();
+  };
+
+  return (
+    <Overlay id="modal-overlay" onClick={onClickOvrlHandle}>
+      <ModalDiv>
+        <CloseModalBtn>
+          <CloseBtn onHandleClick={closeModal} />
+        </CloseModalBtn>
+
+        <ContentWrap>
+          <ModalTtl>Ви підтверджуєте {text}?</ModalTtl>
+
+          <Text>{subText}</Text>
+
+          <BtnWrapper>
+            <Button
+              onClickHandler={onCancelClickHandle}
+              btnText="Скасувати"
+              autofocus={true}
+            />
+            <Button
+              onClickHandler={onOkClickHandle}
+              btnText="Прийняти"
+              autofocus={false}
+            />
+          </BtnWrapper>
+        </ContentWrap>
+      </ModalDiv>
+    </Overlay>
+  );
+}
+
+export default ChoiceModal;
--- src/components/DiaryProductListItem/DiaryProductListItem.jsx
+++ src/components/DiaryProductListItem/DiaryProductListItem.jsx
@@ -1,11 +1,15 @@
 import { GrClose } from 'react-icons/gr';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Product, ProductInfo } from './DiaryProductListItemStyle';
 import { diaryPerDayOperation } from '../../redux/app/diaryPerDay';
 import { diarySelectors } from '../../redux/app/diaryPerDay/';
+import ChoiceModal from '../../components/ChoiceModal';
 
 export const DiaryProductListItem = ({ product }) => {
   const { weightGrm, _id } = product;
+
+  const [showModal, setShowModal] = useState(false);
 
   const dispatch = useDispatch();
   const date = useSelector(diarySelectors.getCurrentDate);
@@ -15,19 +19,37 @@ export const DiaryProductListItem = ({ product }) => {
     date: date,
   };
 
+  const handleDelete = () => {
+    setShowModal(true);
+  };
+
+  const choiceHandler = answer => {
+    if (answer) {
+      dispatch(diaryPerDayOperation.actionDeleteProduct(payload));
+    }
+  };
+
+  const closeModalHandle = () => {
+    setShowModal(false);
+  };
+
   return (
     <Product>
+      {showModal && (
+        <ChoiceModal
+          text={'видалення цього продукту'}
+          choiceHandler={choiceHandler}
+          closeModalHandle={closeModalHandle}
+          subText={product.product.title.ua}
+        />
+      )}
+
       <ProductInfo>
         <div>{product.product.title.ua}</div>
         <div>{weightGrm} g</div>
         <div>{product.product.calories}kcal</div>
       </ProductInfo>
-      <button
-        type="button"
-        onClick={() =>
-          dispatch(diaryPerDayOperation.actionDeleteProduct(payload))
-        }
-      >
+      <button type="button" onClick={handleDelete}>
         <GrClose />
       </button>
     </Product>
