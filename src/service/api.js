73f7434305e282dd442eb8c249b4cdab096d73c8
Marina Tripetska <marina.tripetskaya@gmail.com>
2023-04-09 19:14:08 +0200

    fix: correct api path

--- src/service/api.js
+++ src/service/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'https://slim-mom-backend-ylpc.onrender.com/api/v1',
-  baseURL: 'http://127.0.0.1:5050/api/v1/',
+  baseURL: 'https://slim-mom-backend-ylpc.onrender.com/api/v1',
+  // baseURL: 'http://127.0.0.1:5050/api/v1/',
   headers: {
     'Content-Type': 'application/json',
   },
